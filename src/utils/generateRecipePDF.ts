ef37fbc7ed7941c98081c724959bfe03e291ffdf
tomas-sedlak <t.sedlak.24@gmail.com>
2024-12-16 22:31:23 +0100

    Lazy fix of fractions destroying styling of PDF

--- src/utils/generateRecipePDF.ts
+++ src/utils/generateRecipePDF.ts
@@ -2,6 +2,25 @@ import jsPDF from 'jspdf';
 import { snakeCase } from 'lodash';
 import RecipeData from '../types/RecipeTypes';
 
+const convertFractions = (text: string): string => {
+    return text
+        .replace(/½/g, '1/2')
+        .replace(/⅓/g, '1/3')
+        .replace(/⅔/g, '2/3')
+        .replace(/¼/g, '1/4')
+        .replace(/¾/g, '3/4')
+        .replace(/⅕/g, '1/5')
+        .replace(/⅖/g, '2/5')
+        .replace(/⅗/g, '3/5')
+        .replace(/⅘/g, '4/5')
+        .replace(/⅙/g, '1/6')
+        .replace(/⅚/g, '5/6')
+        .replace(/⅛/g, '1/8')
+        .replace(/⅜/g, '3/8')
+        .replace(/⅝/g, '5/8')
+        .replace(/⅞/g, '7/8');
+};
+
 const loadImage = async (url: string): Promise<HTMLImageElement> => {
     return new Promise((resolve, reject) => {
         const img = new Image();
@@ -92,7 +111,7 @@ export const generateRecipePDF = async (recipe: RecipeData) => {
     doc.setFontSize(12);
     doc.setFont('helvetica', 'normal');
     recipe.ingredients.forEach(ingredient => {
-        const bulletedText = `• ${ingredient}`;
+        const bulletedText = `• ${convertFractions(ingredient)}`;
         const lines = doc.splitTextToSize(bulletedText, contentWidth - 5);
 
         addNewPageIfNeeded(lines.length * lineHeight);
@@ -112,7 +131,7 @@ export const generateRecipePDF = async (recipe: RecipeData) => {
     doc.setFontSize(12);
     doc.setFont('helvetica', 'normal');
     recipe.instructions.forEach((instruction, index) => {
-        const numberedText = `${index + 1}. ${instruction}`;
+        const numberedText = `${index + 1}. ${convertFractions(instruction)}`;
         const lines = doc.splitTextToSize(numberedText, contentWidth - 5);
 
         addNewPageIfNeeded(lines.length * lineHeight);
