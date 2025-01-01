e3ce52e677daf09d04050448eaf2e5afee0be73d
tomas-sedlak <t.sedlak.24@gmail.com>
2025-01-01 14:37:00 +0100

    FIxed capital letters in recipe titles

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,9 +4,11 @@ const getRecipeName = (name: string) => {
 
 const formatArray = (arr: string[]) => {
     if (arr.length === 1) return arr[0];
-    const firsts = getRecipeName(arr.slice(0, arr.length - 1).join(', '));
+    const firsts = arr.slice(0, arr.length - 1)
+        .map((item) => getRecipeName(item))
+        .join(', ');
     const last = getRecipeName(arr[arr.length - 1]);
     return firsts + ' and ' + last;
 }
 
-export { getRecipeType, getRecipeName, formatArray };
+export { getRecipeName, formatArray };
